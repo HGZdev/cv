f238b1b6ee64c0e889de1d0c393f3555c25f20b4
HGZdevi <HGZdevi@gmail.com>
2025-01-03 23:16:13 +0100

    VITE_BASE_URL null case

--- src/client/Components/getConfig.ts
+++ src/client/Components/getConfig.ts
@@ -1,6 +1,6 @@
 const getConfig = () => {
   const {VITE_BASE_URL, VITE_HASH_ROUTER} = import.meta.env;
-  const BASE_URL = VITE_HASH_ROUTER ? "" : VITE_BASE_URL;
+  const BASE_URL = VITE_HASH_ROUTER ? "" : VITE_BASE_URL ? VITE_BASE_URL : "";
 
   return {
     ...import.meta.env,
