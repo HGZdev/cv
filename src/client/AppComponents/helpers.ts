e3be164c2bae369901507e4ee0466321e07bd249
HGZdevi <HGZdevi@gmail.com>
2025-03-24 10:34:42 +0100

    styling fixes

--- src/client/AppComponents/helpers.ts
+++ src/client/AppComponents/helpers.ts
@@ -1,3 +1,6 @@
+import {format, parseISO} from "date-fns";
+import {enUS, pl} from "date-fns/locale";
+
 export const encodeString = (phone: string): string => {
   return phone
     .split("")
@@ -11,3 +14,34 @@ export const decodeString = (encoded: string): string => {
     .map((char) => String.fromCharCode(char.charCodeAt(0) - 1))
     .join("");
 };
+
+/**
+ * Formats a given date into the specified format with locale support.
+ * @param date - The date to format (string, Date, or number).
+ * @param dateFormat - The format string (default: 'yyyy-MM-dd HH:mm:ss').
+ * @param locale - The locale to use ('en' for English, 'pl' for Polish, default is 'en').
+ * @returns Formatted date string.
+ */
+export function formatDate(
+  date: string | Date | number,
+  dateFormat: string = "yyyy-MM-dd HH:mm:ss",
+  locale: "en" | "pl" = "en"
+): string {
+  try {
+    let parsedDate: Date;
+
+    if (typeof date === "string") {
+      parsedDate = parseISO(date); // Parse string date to Date object
+    } else {
+      parsedDate = new Date(date);
+    }
+
+    // Select the correct locale
+    const selectedLocale = locale === "pl" ? pl : enUS;
+
+    return format(parsedDate, dateFormat, {locale: selectedLocale});
+  } catch (error) {
+    console.error("Invalid date:", error);
+    return "Invalid Date";
+  }
+}
--- src/client/Pages/Public/Resume.tsx
+++ src/client/Pages/Public/Resume.tsx
@@ -9,7 +9,6 @@ import {
   educationBlocks,
   fileName,
   fullName,
-  github,
   hobbiesSection,
   jobBlock,
   jobTitle,
@@ -22,6 +21,8 @@ import {
 } from "../../contentDb";
 import {MdOutlineStar, MdOutlineStarBorder} from "react-icons/md";
 import {I18nLang, useLang} from "../../../../lib/i18n";
+import {formatDate} from "../../AppComponents/helpers";
+import {GiCheckMark} from "react-icons/gi";
 
 const {VITE_BASE_URL} = getConfig();
 
@@ -34,12 +35,14 @@ export const getDateRangeString = ({
   startDate: string;
   endDate?: string;
   getText: Function;
-  lang?: string;
+  lang?: "en" | "pl";
 }) => {
-  return `${startDate} ${
+  return `${formatDate(startDate, "MMM yyyy", forceLang)} ${
     endDate
-      ? endDate !== startDate && ` – ${endDate}`
-      : ` – ${getText("date_ongoing", forceLang)}`
+      ? endDate !== startDate
+        ? ` – ${formatDate(endDate, "MMM yyyy", forceLang)}`
+        : ""
+      : ` – ${getText("date_present", forceLang)}`
   }`;
 };
 
@@ -52,53 +55,83 @@ const LeftHeadline = ({
 }) => {
   const {getText} = useLang();
   return (
-    <h2 className="bg-onSecondaryResume text-[12px] font-extralight leading-tight px-4 py-2">
+    <h2 className="bg-onSecondaryResume text-[12px] font-extralight leading-tight px-3 py-2">
       {getText(headlineKey, lang)}
     </h2>
   );
 };
 
-const LeftRatingSection = ({
+const IconAndLabel = ({
+  Icon,
+  labelKey,
+  lang,
+  iconProps,
+}: {
+  Icon: any;
+  labelKey: string;
+  lang: I18nLang;
+  iconProps?: any;
+}) => {
+  const {getText} = useLang();
+  return (
+    <div className="flex gap-2">
+      <Icon size={14} {...iconProps} />
+      <span className="text-[10px] font-extralight">
+        {getText(labelKey, lang)}
+      </span>
+    </div>
+  );
+};
+
+const Rating = ({stars = 0}) => (
+  <div className="flex gap-1">
+    {Array.from({length: stars}, (_, i) => (
+      <MdOutlineStar
+        key={`star-${i}`}
+        size={12}
+        className="text-onPrimaryResume"
+      />
+    ))}
+    {Array.from({length: 5 - stars}, (_, i) => (
+      <MdOutlineStarBorder
+        key={`empty-star-${i}`}
+        size={12}
+        className="text-onBackground"
+      />
+    ))}
+  </div>
+);
+
+const LeftSkillsSection = ({
   headlineKey,
   blocks,
   lang,
 }: SectionProps & {lang: I18nLang}) => {
-  const {getText} = useLang();
   return (
     <div>
       <LeftHeadline headlineKey={headlineKey} lang={lang} />
-      <div className="flex flex-col py-3 px-4 gap-2">
+      <div className="flex flex-col py-3 px-3 ">
         {blocks.map((b, i) => (
-          <div key={i} className="flex flex-col gap-[0.1rem]">
-            {b.list
-              .filter((l) => !l.hidden)
-              .map(({titleKey, stars = 0, Icon}, j) => (
-                <div key={j} className="flex justify-between">
-                  <div className="flex text-onPrimary gap-2">
-                    {Icon && <Icon size={14} />}
-                    <span className="text-[10px] font-extralight text-center">
-                      {getText(titleKey!, lang)}
-                    </span>
+          <>
+            <div key={i} className="grid grid-cols-2 gap-1 gap-x-2">
+              {b.list
+                .filter((l) => !l.hidden)
+                .map(({titleKey, stars = 0, Icon, iconProps}, j) => (
+                  <div key={j}>
+                    {/* ICON AND LABEL */}
+                    <IconAndLabel
+                      Icon={Icon!}
+                      labelKey={titleKey!}
+                      lang={lang}
+                      iconProps={iconProps}
+                    />
                   </div>
-                  <div className="flex gap-1">
-                    {Array.from({length: stars}, (_, i) => (
-                      <MdOutlineStar
-                        key={`star-${i}`}
-                        size={12}
-                        className="text-onPrimaryResume"
-                      />
-                    ))}
-                    {Array.from({length: 5 - stars}, (_, i) => (
-                      <MdOutlineStarBorder
-                        key={`empty-star-${i}`}
-                        size={12}
-                        className="text-onBackground"
-                      />
-                    ))}
-                  </div>
-                </div>
-              ))}
-          </div>
+                ))}
+            </div>
+            {i < blocks.length - 1 && (
+              <div className="border-b-[1px] border-onPrimaryResume/10 bg-opacity-10 border-dotted my-2" />
+            )}
+          </>
         ))}
       </div>
     </div>
@@ -114,7 +147,7 @@ const LeftTextSection = ({
   return (
     <div>
       <LeftHeadline headlineKey={headlineKey} lang={lang} />
-      <div className="flex flex-col px-4 py-3 gap-[0.3rem]">
+      <div className="flex flex-col px-3 py-3 gap-2">
         {blocks.map((b) =>
           b.list
             .filter((l) => !l.hidden)
@@ -144,24 +177,26 @@ const LeftTextSection = ({
 const LeftBar = ({lang}: {lang: I18nLang}) => {
   const imagePath = `${VITE_BASE_URL}/images/portrait2.webp`;
   return (
-    <div className="bg-primaryResume text-onPrimary flex flex-col w-1/3 gap-4 py-4">
+    <div className="bg-primaryResume text-onPrimary flex flex-col w-2/5 gap-3 py-3">
       <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-1 mx-4">
+        {/* TITLE */}
+        <div className="flex flex-col gap-1 mx-3">
           <h1 className="text-[30px] font-light leading-[1.2]">{fullName}</h1>
           <p className="text-[16px] font-extralight leading-tight">
             {jobTitle}
           </p>
         </div>
+        {/* IMAGE */}
         <div
-          className="bg-no-repeat bg-[calc(50%+10px)_center] bg-[length:130%] mx-4"
+          className="bg-no-repeat bg-[calc(50%+10px)_calc(50%-10px)] bg-[length:130%] mx-3"
           style={{backgroundImage: `url('${imagePath}')`}}
         >
-          <div className="w-full h-60"></div>
+          <div className="w-full h-[15rem]"></div>
         </div>
       </div>
       <div>
         <LeftTextSection {...personalSection} lang={lang} />
-        <LeftRatingSection
+        <LeftSkillsSection
           headlineKey="skills_headline"
           blocks={[
             ...langSection.blocks,
@@ -185,19 +220,13 @@ const RightHeadline = ({
 }) => {
   const {getText} = useLang();
   return (
-    <div className="py-1 my-2 border-b-primaryResume border-b-[1px] border-solid">
-      <h2 className="border-y-2 text-[14px] font-light leading-tight ml-1">
+    <div className="py-1 my-2 border-t-transparent border-b-onSecondaryResume/30 border-b-[1px] border-solid">
+      <h2 className=" text-[14px] font-light leading-tight ml-1">
         {getText(headlineKey, lang)}
       </h2>
     </div>
   );
 };
-
-const TextBubble = ({text}: {text: string}) => (
-  <div className="flex bg-[#e8edff] rounded-xl m-[0.1rem] px-2 py-1 shadow-md">
-    <span className="text-[8px] font-normal">{text}</span>
-  </div>
-);
 
 const AboutSection = ({lang}: {lang: I18nLang}) => {
   const {getText} = useLang();
@@ -206,52 +235,45 @@ const AboutSection = ({lang}: {lang: I18nLang}) => {
       <p>{getText("about_text_paragraph_1", lang)}</p>
       <p>{getText("about_text_paragraph_2", lang)}</p>
       <p>{getText("about_text_paragraph_3", lang)}</p>
-      <p>
-        {getText("about_github_invitation_text", lang)}{" "}
-        <a
-          target="_blank"
-          href={github}
-          className="text-onSecondary hover:text-onPrimary transition-transform duration-200 underline underline-offset-2"
-        >
-          {getText("about_github_link_text", lang)}
-        </a>
-        .
-      </p>
     </div>
   );
 };
 
+const Li = ({children}) => (
+  <li className="flex py-[0.1rem] gap-[3px] text-body-small ">
+    <GiCheckMark className="text-[#7085c9]" />
+    <span className="font-normal">{children}</span>
+  </li>
+);
+
 const KeySkillsSection = ({lang}: {lang: I18nLang}) => {
-  const {getText} = useLang();
-  const keyWords = [
-    "skills_frontend",
-    "skills_backend",
-    "technologies_typescript_title",
-    "technologies_react_title",
-    "technologies_vite_title",
-    "technologies_webpack_title",
-    "technologies_tailwind_title",
-    "technologies_styled_components_title",
-    "technologies_nodejs_title",
-    "technologies_graphql_title",
-    "technologies_sqlite_title",
-    "technologies_jest_title",
-    "technologies_vitest_title",
-    "technologies_testing_library_title",
-    "tools_git_title",
-    "tools_github_title",
-    "skills_cicd_title",
-    "tools_figma_title",
-    "skills_pm",
-    "skills_client_liaison",
-  ];
   return (
     <div>
       <RightHeadline headlineKey="key_skills_headline" lang={lang} />
-      <div className="flex flex-wrap gap-1">
-        {keyWords.map((key) => (
-          <TextBubble key={key} text={getText(key, lang)} />
-        ))}
+      <div className="flex px-2">
+        <ul className="w-full">
+          <Li>Frontend Focus</Li>
+          <Li>JavaScript / TypeScript</Li>
+          <Li>React.js</Li>
+          <Li>HTML / CSS</Li>
+          <Li>Unit / Integration / E2E Testing</Li>
+          <Li>SEO</Li>
+        </ul>
+        <ul className="w-full">
+          <Li>Backend Experience</Li>
+          <Li>Node.js</Li>
+          <Li>SQL / SQLite</Li>
+          <Li>API Integration: REST / GraphQL</Li>
+          <Li>Git / Github</Li>
+          <Li>Webpack / Vite</Li>
+        </ul>
+        <ul className="w-full">
+          <Li>Adaptability & Fast Learning</Li>
+          <Li>Project Management Skills</Li>
+          <Li>Interpersonal Communication</Li>
+          <Li>Client Liaison</Li>
+          <Li>Pixel-perfect Approach</Li>
+        </ul>
       </div>
     </div>
   );
@@ -268,32 +290,44 @@ const RightSection = ({
 }) => {
   const {getText} = useLang();
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ">
       <RightHeadline headlineKey={headlineKey} lang={lang} />
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-2 ml-1">
         {blocks
           .filter((b) => !b.hidden)
           .map(
             ({
               titleKey,
               subtitleKey,
-              startDate,
+              startDate = "",
               endDate,
               textKey,
               listKeys,
+              location,
             }) => (
               <div className="flex gap-2" key={titleKey}>
-                <div className="flex w-12 pl-1 pt-1">
-                  <span className="font-mono font-light text-[8px] text-primaryResume">
+                {/* <div className="flex w-12 pl-1">
+                  <span className="font-mono font-light text-[7px] text-primaryResume">
                     {getDateRangeString({startDate, endDate, getText, lang})}
                   </span>
-                </div>
+                </div> */}
                 <div className="flex flex-col flex-1">
-                  <span className="text-primaryResume text-[12px] font-normal leading-tight">
-                    {getText(titleKey!, lang)}
-                  </span>
-                  <span className="text-primaryResume text-[14px] font-semibold">
+                  {/* POSITION */}
+                  <span className="text-primaryResume text-[12px] font-semibold uppercase">
                     {subtitleKey && getText(subtitleKey!, lang)}
+                  </span>
+                  {/* TITLE */}
+                  <span className="text-[10px] font-normal leading-tight">
+                    <span className="text-[#7085c9]">
+                      {getDateRangeString({startDate, endDate, getText, lang})}
+                    </span>
+                    {" | "}
+                    {[
+                      titleKey ? getText(titleKey!, lang) : "",
+                      location ? getText(location, lang) : "",
+                    ]
+                      .filter(Boolean)
+                      .join(", ")}
                   </span>
                   {textKey && (
                     <span className="text-primaryResume text-[10px] font-light">
@@ -334,8 +368,8 @@ const ConsentSection = ({lang}: {lang: I18nLang}) => {
 
 const RightBar = ({lang}: {lang: I18nLang}) => (
   <div className="flex flex-col justify-between">
-    <AboutSection lang={lang} />
     <div className="flex flex-col">
+      <AboutSection lang={lang} />
       <KeySkillsSection lang={lang} />
       <RightSection
         headlineKey="work_experience_headline"
@@ -358,7 +392,7 @@ const RightBar = ({lang}: {lang: I18nLang}) => (
 );
 
 const CVPanel = ({lang}: {lang: I18nLang}) => (
-  <div className="flex bg-white h-[293mm] w-[210mm] p-4 gap-4">
+  <div className="flex bg-white h-[293mm] w-[210mm] p-4 gap-3">
     <LeftBar lang={lang} />
     <RightBar lang={lang} />
   </div>
@@ -387,17 +421,10 @@ const Resume = () => {
         >
           <PiPrinter />
         </button>
-
-        {/* <a
-          className="bg-onPrimary text-primaryResume rounded-sm px-2"
-          href={`cv${pdfPath}`}
-        >
-          <MdOutlineFileDownload />
-        </a> */}
       </div>
       <div ref={cvRef} className="bg-white">
         <CVPanel lang={"en"} />
-        <CVPanel lang={"pl"} />
+        {/* <CVPanel lang={"pl"} /> */}
       </div>
     </BgContainer>
   );
